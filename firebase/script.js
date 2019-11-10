7226cda05829aa8722a2dbe776831c0eb33a4b3c
Brenden Roland <bmr549@lindenwood.edu>
2019-11-09 18:52:35 -0600

    Code cleaning and documentation

--- firebase/script.js
+++ firebase/script.js
@@ -1,53 +1,45 @@
-const db = firebase.firestore();
-var currentUser;
+const db = firebase.firestore(); // accessable instance variable for Firebase Firestore
+var currentUser; // the currently connected user
 
 /**
- * HandleFileSelect Function
- * 
- * Grabs the selected file from the file upload box
- * 
- * @param {*} evt - the file event used to access the file information
+ * Receives a file InputEvent event and forwards the received input picture to Firebase
+ * and other parts of the program.
+ * @param {InputEvent} inputEvent - the file event used to access the file information
  */
-function handleFileSelect(evt) {
-  evt.stopPropagation();
-  evt.preventDefault();
-  var file = evt.target.files[0];
-  console.log("File to Upload: " + file);
-  pushFileToFirebase(file);
+function handleFileSelect(inputEvent) {
+  inputEvent.stopPropagation(); // prevents other nodes from receiving this event
+  inputEvent.preventDefault(); // prevents default results when no action is taken
+  pushFileToFirebase(inputEvent.target.files[0]);
 }
 
 /**
- * PushFileToFirebase Function
- * 
- * Pushes a single file to firebase storage
- * 
- * @param {*} file - the file to push to firebase storage
+ * Pushes a single image file to the Firebase storage module's /images/... reference
+ * @param {object} file - the image file to be pushed to Firebase
  */
 function pushFileToFirebase(file) {
-  var storageRef = firebase.storage().ref('images/' + file.name)
+  // a storage reference is needed to specify exactly where the file is going
+  // it can be thought of as a filepath with directories of where the file is
+  // going to be stored
+  var storageRef = firebase.storage().ref("images/" + file.name)
 
-  // Push the file to firebase storage
-  // [START oncomplete]
+  // push the file to firebase storage
   storageRef.put(file).then(function(snapshot) {
-    console.log('Uploaded', snapshot.totalBytes, 'bytes.');
+    console.log("Uploaded file " +  file.name + " with size of " + snapshot.totalBytes + " bytes.");
     
-    // let's grab the download url
+    // let's grab the download url...
     snapshot.ref.getDownloadURL().then(function(url) {
-      // and attach that image url to the user's firestore
+      // ...and attach that image url to the users' Firestore subcollections
       attachImageToUser(url);
     });
-
   }).catch(function(error) {
-    // [START onfailure]
-    console.error('Upload failed:', error);
-    // [END onfailure]
+    console.error("Upload failed:", error);
   });
 }
 
 /**
- * 
- * 
- * @param {*} src 
+ * Creates a Firebase database document with a field containing the Firebase
+ * storage download URL for an image added by the user.
+ * @param {string} src - Firebase Storage download URL for an image
  */
 function attachImageToUser(src) {
   db.collection("users").doc(currentUser.email).collection("images").add({
@@ -60,11 +52,9 @@ function attachImageToUser(src) {
 }
 
 /**
- * AddImageToGallery Function
- * 
- * Adds an image to the page utilizing 
- * 
- * @param {*} src 
+ * Writes an image to the viewable HTML page using a given downloaded source URL
+ * from Firebase Storage
+ * @param {string} src - Firebase Storage download URL for an image
  */
 function addImageToGallery(src) {
   var galleryHTML = document.getElementById("gallery");
@@ -74,9 +64,8 @@ function addImageToGallery(src) {
 }
 
 /**
- * PushNewUserToFirebase Function
- * 
- * @param {*} user 
+ * Creates a new document under the users collection in Firestore for new users
+ * @param {object} user 
  */
 function pushNewUserToFirebase(user) {
   db.collection("users").doc(user.email).set({
@@ -89,7 +78,8 @@ function pushNewUserToFirebase(user) {
 }
 
 /**
- * DiplayGallery Function
+ * Pulls all the images assigned to a user from Firestore and calls for the images 
+ * to be written to the viewable HTML page
  */
 function displayGallery() {
   db.collection("users").doc(currentUser.email).collection("images").get().then(function(querySnapshot) {
@@ -104,9 +94,9 @@ function displayGallery() {
 }
 
 /**
- * UserExists Function
- * 
- * @param {*} email 
+ * Checks to see if a given user has an document in the Firestore users collection and if no,
+ * creates a new document for them
+ * @param {object} user - User JS object retrieved from the Google login API
  */
 function userExists(user) {
   var exists = db.collection("users").doc(user.email).get().then(function(doc) {
@@ -115,33 +105,30 @@ function userExists(user) {
 }
 
 /**
- * OnLoad Function
- * 
  * Runs when the window is loaded to preform initial setup code such as event listeners.
  */
 window.onload = function() {
 
-  // add an event listen to the file selector button
-  this.document.getElementById('file').addEventListener('change', handleFileSelect, false);
-  this.document.getElementById('galleryBtn').addEventListener('click', this.displayGallery);
+  // add event listeners to the file selector and gallery buttons
+  this.document.getElementById("file").addEventListener("change", handleFileSelect, false);
+  this.document.getElementById("galleryBtn").addEventListener("click", displayGallery);
 
-  var auth = new firebase.auth.GoogleAuthProvider();
+  var providerGoogle = new firebase.auth.GoogleAuthProvider(); // login with Google
 
-  firebase.auth().useDeviceLanguage();
-  firebase.auth().signInWithPopup(auth).then(function(result) {
+  firebase.providerGoogle().useDeviceLanguage();
+  firebase.providerGoogle().signInWithPopup(providerGoogle).then(function(result) {
     
-    // this gives us a Google Access Token for accessing the Google api
+    // this gives us a Google Access Token for accessing the Google API
     var token = result.credential.accessToken;
 
-    // the signed-in user info
-    var user = result.user;
-    currentUser = user;
+    // save the signed-in user info
+    currentUser = result.user;
 
     // check to see if the user is already in our Firestore
     // if they are not, then we need to add them
     // we store userdata in Firestore to enable picture references
     // to be linked with a specific user
-    userExists(user);
+    userExists(currentUser);
   }).catch(function(error) {
 
     // handle errors here
