1aa552ad909321443b36d2576d9ef77313cdc4bd
Oliver Johnson <oa_johnson@outlook.com>
2020-07-03 01:23:12 -0500

    Created LossFunction class

--- main.py
+++ main.py
@@ -15,22 +15,8 @@ import torchvision.models as models  # train/load pre-trained models
 
 import copy  # to deep copy models
 
-# FUNCTION DEFINITIONS
-
-
-class ContentLoss(nn.Module):
-
-    def __init__(self, target,):
-        super(ContentLoss, self).__init__()
-        # we 'detach' the target content from the tree used
-        # to dynamically compute the gradient: this is a stated value,
-        # not a variable. Otherwise the forward method of the criterion
-        # will throw an error.
-        self.target = target.detach()
-
-    def forward(self, input):
-        self.loss = F.mse_loss(input, self.target)
-        return input
+from LossFunctions import ContentLoss
+from LossFunctions import StyleLoss
 
 
 # create a module to normalize input image so we can easily put it in a
@@ -47,18 +33,6 @@ class Normalization(nn.Module):
     def forward(self, img):
         # normalize img
         return (img - self.mean) / self.std
-
-
-class StyleLoss(nn.Module):
-
-    def __init__(self, target_feature):
-        super(StyleLoss, self).__init__()
-        self.target = gram_matrix(target_feature).detach()
-
-    def forward(self, input):
-        G = gram_matrix(input)
-        self.loss = F.mse_loss(G, self.target)
-        return input
 
 
 def image_loader(image_name):
@@ -83,20 +57,6 @@ def imsave(path, unloader, tensor):
     image = image.squeeze(0)
     image = unloader(image)
     image.save(path)
-
-
-def gram_matrix(input):
-    a, b, c, d = input.size()  # a=batch size(=1)
-    # b=number of feature maps
-    # (c,d)=dimensions of a f. map (N=c*d)
-
-    features = input.view(a * b, c * d)  # resise F_XL into /hat F_XL
-
-    G = torch.mm(features, features.t())  # compute the gram product
-
-    # we normalize the values of the gram matrix
-    # by dividing the number of element in each feature maps
-    return G.div(a * b * c * d)
 
 
 def get_style_model_and_losses(cnn, normalization_mean, normalization_std,
